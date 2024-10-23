49be5fe48decddc9eb99c50b17d71fbf8268b4b1
Shane McDaniel <shaneam@gmail.com>
2024-10-23 10:05:57 -0400

    Pull out special cards for reuse

--- BGATichu/scripts/log_extractor.test.ts
+++ BGATichu/scripts/log_extractor.test.ts
@@ -1,9 +1,15 @@
 import LogExtractor from "./log_extractor";
 
+const specialCards = [
+    // Dog and Dragon get the full name as they're they're the only initial
+    // letter collision, rest get the inital letter.
+    ["DRAGON", "DRAGON"], ["phoenix", "p"], ["dog", "dog"], ["MaHjOng", "M"], ["queen", "q"]
+];
+
 describe('getSinglePlayCards', () => {
     const logExtractor = new LogExtractor();
 
-    it.each([["2", "2"], ["10", "10"], ["DRAGON", "DRAGON"], ["queen", "q"]])
+    it.each([["2", "2"], ["10", "10"], ["queen", "q"], ...specialCards])
         ('parses %s as %s', (card, want) => {
             expect(logExtractor.getSinglePlayCards("Plays " + card)).toStrictEqual([want]);
         });
