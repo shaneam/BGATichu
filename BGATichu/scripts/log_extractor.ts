19f7d01330c5385c1ca8388e2a6829ac61d15148
Shane McDaniel <90928876+shanemc-goog@users.noreply.github.com>
2025-02-03 12:05:06 -0500

    Add comments

--- BGATichu/scripts/log_extractor.ts
+++ BGATichu/scripts/log_extractor.ts
@@ -1,3 +1,12 @@
+/* Regex extractions for each kind of hand played.
+
+  We extract only the first letter of each card with the exception of the dragon/dog in the single card play as they
+  have the same starting letter.
+
+  Note: The phoenix is not always discernible from the logs as it is not always specified such as in the middle of a
+  straight, or part of a pair/triple.  This is a limitation of the log approach.
+*/
+
 const newRoundRE = /A new round starts/i
 const playsConsecutivePairsRE = /consecutive doubles from (\d{1,2}|\w)\w* to (\d{1,2}|\w)/i
 const playsPairsRE = /plays Pair of (\d{1,2}|\w)/i
@@ -8,15 +17,38 @@ const playsQuadBombRE = /plays Bomb of four (\d{1,2}|\w)/i
 const playsStraightBombRE = /plays Straight flush bomb starting from (\d{1,2}|\w)\w* to (\d{1,2}|\w)/i
 const playsSingleRE = /plays (\d{1,2}|dragon|dog|[a-z])/i
 
+// The canonical representation of each standard card and their relative order, lowest to highest.
 const cardsRanked = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 
 export = LogExtractor;
 
+/**
+    Utility for extracting cards from log messages and cumulative container for extracted cards.
+
+    It is intended that all log messages are passed through the class.
+    The utility detects when a new hand starts and resets as such.
+ */
 class LogExtractor {
     private cardsUsed = new Map<string, number>();
+    /**
+        View of the currently extracted cards.
+
+        Response is a Map from the single letter representation of a card to how many times it has be observed.
+
+        Representation is that in cardsRanked for standard cards and
+        * P - Phoenix
+        * M - Mahjong / 1
+        * DRAGON
+        * DOG
+
+        Single letters don't suffice from Dragon/Dog so the full name is used.
+     */
     getCardsUsed(): Map<string, number> {
         return this.cardsUsed;
     }
+
+    // The following getXPlayCards methods process a log message for the kind of hand indicated.
+
     getSinglePlayCards(log: string): string[] {
 
         let match = log.match(playsSingleRE);
@@ -188,14 +220,17 @@ class LogExtractor {
     }
 
 
+/** Processes a single line of log text, updating the internal record appropriately. */
     processLogText(log: string) {
         if (newRoundRE.test(log)) {
             this.cardsUsed.clear();
-            console.log("Reseting card counts");
+            console.log("Resetting card counts");
             return;
         }
 
         let cards = this.getConsecutivePairsCards(log);
+        // The order of processing is important as some messages can be construed as others.
+        // The Single card play especially as it lacks defining characteristics the others have.
         if (cards.length == 0) {
             cards = this.getPairPlayCards(log);
         }
